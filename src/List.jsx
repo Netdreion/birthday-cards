07b6c296f37ad29d0173afb33d1b3750b24e9578
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-08-07 12:57:18 -0400

    List component set-up

--- src/List.jsx
+++ src/List.jsx
@@ -2,10 +2,21 @@ import { useState } from "react";
 
 import "./App.css";
 
-function List() {
+function List({ person }) {
   return (
     <>
-      <h1>birthday Card</h1>
+      {person.map((bday) => {
+        const { name, age, id, image } = bday;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
     </>
   );
 }
