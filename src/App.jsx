a4fa4d4664847fca4e49f4c3d208ee241d87248f
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-08-07 12:36:59 -0400

    App component set-up

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,13 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>birthday Card</h1>
-    </>
+    <main>
+      <section className="container">
+        <h3> 0 birthday today</h3>
+        <List />
+        <button onClick={() => console.log("clearn me ")}> clear</button>
+      </section>
+    </main>
   );
 }
 
